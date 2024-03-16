9a8905f5baab3e248b4975c553449e1392d00017
Anindya Mondal <anindyam.jan@gmail.com>
2024-03-16 21:24:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const handle = document.querySelector('.handle');
     const slider = document.querySelector('.slider');
-    const container = document.querySelector('.image-compare-container');
-
     let isPressedDown = false;
     let startX;
-    let offsetLeft;
 
     const mouseDownHandler = function(e) {
         isPressedDown = true;
         startX = e.clientX;
-        offsetLeft = handle.offsetLeft;
         document.addEventListener('mousemove', mouseMoveHandler);
         document.addEventListener('mouseup', mouseUpHandler);
     };
 
     const mouseMoveHandler = function(e) {
         if (!isPressedDown) return;
-        const x = e.clientX;
-        const walk = (x - startX) * 2; // Multiplied by 2 due to the slider image width being 200%
-        let newLeft = offsetLeft + walk;
-        if (newLeft < 0) {
-            newLeft = 0;
-        } else if (newLeft > container.offsetWidth - handle.offsetWidth) {
-            newLeft = container.offsetWidth - handle.offsetWidth;
-        }
-        handle.style.left = newLeft + 'px';
-        slider.style.width = newLeft + 'px';
+        const dx = e.clientX - startX;
+        let newWidth = Math.max(0, Math.min(slider.offsetWidth + dx, slider.parentElement.offsetWidth));
+        slider.style.width = `${newWidth}px`;
+        handle.style.left = `${newWidth}px`;
+        startX = e.clientX;
     };
 
     const mouseUpHandler = function() {
